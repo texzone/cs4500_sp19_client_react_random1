12e105ef8d9f00dc193a3a4c784571beb75cf314
texzone <ali.elgabri@gmail.com>
2019-03-15 20:40:50 -0400

    Added User CRUD operations in details pg and summary tbl

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import UserService from '../services/UserService'
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
+
 class Users extends React.Component {
     constructor(props) {
-        super(props)
-        this.userService = UserService.getInstance()
+        super(props);
+        this.userService = UserService.getInstance();
         this.state = {
+            editingUser: {
+                username: '',
+                password: '',
+                firstName: '',
+                lastName: ''
+            },
             users: []
         }
     }
+
     componentDidMount() {
         this.userService
             .findAllUsers()
@@ -17,12 +26,89 @@ class Users extends React.Component {
                 })
             )
     }
+
+    createUser = (user) =>
+        this.userService
+            .createUser(user)
+            .then(this.props.history.push("/admin/users/"));
+
+    saveUser = function (user) {
+        if (user.id) {
+            this.updateUser(user);
+        } else {
+            this.createUser(user);
+        }
+    };
+
+    updateUser = (user) =>
+        this.userService
+            .updateUser(user)
+            .then(this.props.history.push("/admin/users/"));
+
+    deleteUser = (user) =>
+        this.userService
+            .deleteUser(user)
+            .then(this.props.history.push("/admin/users/"));
+
     render() {
-        return(
+        return (
             <div>
                 <h3>Users</h3>
+
                 <table className="table">
                     <tbody>
+                    <tr>
+                        <td><input onChange={(e) =>
+                            this.setState({
+                                editingUser: {
+                                    id: this.state.editingUser.id,
+                                    username: e.target.value,
+                                    password: this.state.editingUser.password,
+                                    firstName: this.state.editingUser.firstName,
+                                    lastName: this.state.editingUser.lastName
+                                }
+                            })} className="form-control"
+                                   value={this.state.editingUser.username}/></td>
+                        <td><input onChange={(e) =>
+                            this.setState({
+                                editingUser: {
+                                    id: this.state.editingUser.id,
+                                    username: this.state.editingUser.username,
+                                    password: this.state.editingUser.password,
+                                    firstName: e.target.value,
+                                    lastName: this.state.editingUser.lastName
+                                }
+                            })} className="form-control"
+                                   value={this.state.editingUser.firstName}/></td>
+                        <td><input onChange={(e) =>
+                            this.setState({
+                                editingUser: {
+                                    id: this.state.editingUser.id,
+                                    username: this.state.editingUser.username,
+                                    password: this.state.editingUser.password,
+                                    firstName: this.state.editingUser.firstName,
+                                    lastName: e.target.value
+                                }
+                            })} className="form-control"
+                                   value={this.state.editingUser.lastName}/></td>
+                        <td>
+                            <button className="btn btn-success" onClick={() => {
+                                this.saveUser(this.state.editingUser)
+                            }}>Save
+                            </button>
+                            <button className="btn btn-warning" onClick={() => {
+                                this.setState({
+                                    editingUser: {
+                                        username: '',
+                                        password: '',
+                                        firstName: '',
+                                        lastName: ''
+                                    }
+                                });
+                            }}>Clear
+                            </button>
+                        </td>
+                    </tr>
                     {
                         this.state.users
                             .map(user =>
@@ -30,6 +116,16 @@ class Users extends React.Component {
                                     <td>{user.username}</td>
                                     <td>{user.firstName}</td>
                                     <td>{user.lastName}</td>
+                                    <td>
+                                        <button className="btn btn-primary" onClick={() => {
+                                            this.setState({editingUser: user});
+                                        }}>Edit
+                                        </button>
+                                        <button className="btn btn-danger" onClick={
+                                            () => this.deleteUser(user)
+                                        }>Delete
+                                        </button>
+                                    </td>
                                 </tr>
                             )
                     }
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,16 +1,38 @@
-import MIDDLE_TIER_URL from '../Env.js'
 export default class UserService {
     static instance = null;
+
     static getInstance() {
-        if(UserService.instance === null) {
+        if (UserService.instance === null) {
             UserService.instance = new UserService()
         }
         return this.instance
     }
+
     findUserById = userId =>
-        fetch(MIDDLE_TIER_URL + "/api/users/" + userId)
+        fetch(`https://cs4500-sp19-random1.herokuapp.com/api/users/${userId}`)
             .then(response => response.json())
     findAllUsers = () =>
-        fetch(MIDDLE_TIER_URL + "/api/users")
+        fetch(`https://cs4500-sp19-random1.herokuapp.com/api/users`)
             .then(response => response.json())
+    createUser = user =>
+        fetch(`https://cs4500-sp19-random1.herokuapp.com/api/users`,
+            {
+                method: 'POST',
+                body: JSON.stringify(user),
+                headers: {'content-type': 'application/json'}
+            })
+            .then(response => response.json())
+    updateUser = user =>
+        fetch(`https://cs4500-sp19-random1.herokuapp.com/api/users/${user.id}`,
+            {
+                method: 'PUT',
+                body: JSON.stringify(user),
+                headers: {'content-type': 'application/json'}
+            })
+            .then(response => response.json())
+    deleteUser = user =>
+        fetch(`https://cs4500-sp19-random1.herokuapp.com/api/users/${user.id}`,
+            {
+                method: 'DELETE',
+            })
 }
