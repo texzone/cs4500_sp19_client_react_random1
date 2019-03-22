19b5645b5bdc69142b8dd57d915750ef1485dc6a
iamorac <bagley.re@husky.neu.edu>
2019-03-22 10:33:49 -0400

    Team Two- FAQ Pagination refractoring done!

---
+++ src/components/views/FAQPagination.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const FAQPagination = (props) => {
+    let div = (
+        <div>
+            <tr>
+                <th><button id="previous" onClick={() => props.previousFn()}>Previous</button></th>
+                <th><div id="pageButtons"> </div></th>
+                <th><button id="next" onClick={() => props.nextFn()}>Next</button></th>
+            </tr>
+            <tr>
+                <th>
+                    <div id = "dropdown" >
+                        <select id="selector" onChange={() => props.changeItemsPerPageFn()}>
+                            <option value="1" selected="selected">10</option>
+                            <option value="2">25</option>
+                            <option value="3">50</option>
+                            <option value="4">100</option>
+                            <option value="5">All</option></select>
+                    </div>
+                </th>
+            </tr>
+        </div>
+    )
+    props.createButtonsFn()
+    return div;
+}
+
+export default FAQPagination;
