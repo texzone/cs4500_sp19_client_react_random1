27dfa5b9140889b56bb1a6b83b371533c57fb34b
texzone <ali.elgabri@gmail.com>
2019-03-22 18:27:28 -0400

    testing

--- src/Env.js
+++ src/Env.js
@@ -2,6 +2,4 @@ var MIDDLE_TIER_URL = (process.env.NODE_ENV === 'development' ?
         "http://localhost:8080" :
         "https://cs4500-sp19-random1.herokuapp.com")
 
-var HOST_URL = "https://cs4500-sp19-random1.herokuapp.com"
-
 export default MIDDLE_TIER_URL
