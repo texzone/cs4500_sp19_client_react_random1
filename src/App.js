14ae9cd4a6320a567f0d0297ec3028dba740a15f
Nicholas Buquicchio <nbuquicchio@carbonite.com>
2019-03-22 16:00:58 -0400

    Refactor Services to be stateless

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import Admin from './components/Admin'
+import ServiceService from './services/ServiceService'
 
 class App extends Component {
+  constructor(props) {
+      super(props)
+      this.servicesService = ServiceService.getInstance()
+  }
   render() {
     return (
         <div className="container-fluid">
--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,17 +1,6 @@
 import React from 'react'
 import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
-/*import Users from './Users'
-import UserDetails from './UserDetails'
-*/
-import Services from './Services'
-/*
-import ServiceDetails from './ServiceDetails'
-import ServiceCategories from './ServiceCategories'
-import ServiceCategoryDetails from './ServiceCategoryDetails'
-import ServiceQuestions from './ServiceQuestions'
-import ServiceQuestionDetails from './ServiceQuestionDetails'
-import ServiceAnswers from './ServiceAnswers'
-import ServiceAnswerDetails from './ServiceAnswerDetails'*/
+import ServicesContainer from './containers/ServicesContainer'
 import FAQs from './FAQs'
 import FAQDetails from './FAQDetails'
 import FAQAnswers from './FAQAnswers'
@@ -43,7 +32,7 @@ const Admin = () =>
                     <Route
                         path="/admin/services"
                         exact
-                        component={Services}/>
+                        component={ServicesContainer}/>
                     <Route
                         path="/admin/faqs"
                         exact
