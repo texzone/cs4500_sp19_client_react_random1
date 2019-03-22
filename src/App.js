1df324081d53b9bd6c53dd28dd5000ec4f2cb6fb
texzone <ali.elgabri@gmail.com>
2019-03-22 19:46:16 -0400

    added user stateless features and fixed some errors

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React, { Component } from 'react';
-import './App.css';
-import Admin from './components/Admin';
-import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import Admin from './components/Admin'
 
 class App extends Component {
   render() {
     return (
-      <div className="container-fluid">
-        <h1>Service</h1>
-        <Router>
-          <div>
-            <Link to="/admin">Admin</Link>
-            <Route
-                path="/admin"
-                exact
-                component={Admin}/>
-          </div>
-        </Router>
-      </div>
+        <div className="container-fluid">
+          <h1>Random1-ServicesRus</h1>
+          <Router>
+            <div>
+              <Link to="/admin">Admin</Link>
+              <Route
+                  path="/admin"
+                  exact
+                  component={Admin}/>
+            </div>
+          </Router>
+        </div>
     );
   }
 }
