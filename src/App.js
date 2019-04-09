90341e747bb342103614998d8d3bdce80b34025f
Nicholas Buquicchio <nbuquicchio@carbonite.com>
2019-04-09 12:39:06 -0400

    Template registration page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import Register from './components/register/Register'
 import Admin from './components/Admin'
 import HomeContainer from './components/containers/HomeContainer'
 import ServiceProviderNavigator from './components/ServiceProviderNavigator'
@@ -12,11 +13,16 @@ class App extends Component {
           <h1>Random1-ServicesRus</h1>
           <Router>
             <div>
+              <Link to="/register">Register</Link> |
               <Link to="/home">Home</Link> |
               <Link to="/admin">Admin</Link>
               <br/>
               <br/>
               <br/>
+              <Route
+                  path="/register"
+                  exact
+                  component={Register}/>
               <Route
                   path="/home"
                   exact
