89adb64be15a5f686d861e13a057286d4a03e1f8
BuhQuick <nbuquicc@gmail.com>
2019-04-16 17:24:06 -0400

    Remove dynamic URL generation for accessing APIs (#43)

--- src/Env.js
+++ src/Env.js
@@ -1,5 +1,3 @@
-var MIDDLE_TIER_URL = (process.env.NODE_ENV === 'development' ?
-        "http://localhost:8080" :
-        "https://cs4500-sp19-random1.herokuapp.com")
+var MIDDLE_TIER_URL = "https://cs4500-sp19-random1.herokuapp.com"
 
 export default MIDDLE_TIER_URL
